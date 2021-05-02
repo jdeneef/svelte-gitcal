405cdb877a6a460509d1bbf38197ee0a09b74f84
Jurriaan de Neef <jurriaan@deneef.biz>
2021-05-02 14:06:51 +0200

    svelte-gitcal as component initial setup

--- README.md
+++ README.md
@@ -1,29 +1,42 @@
-# svelte-ts-component-template
+# Svelte-gitcal component 
+A Github lookalike calender svelte component
 
-A base for building shareable Svelte components. Clone it with [degit](https://github.com/Rich-Harris/degit):
+Demo: [de-neef.net/gitlog](https://de-neef.net/gitlog)
+
+Based upon: 
+- [svelte-ts-component-template](https://github.com/patrickroberts/svelte-ts-component-template) but updated
+- which in turn is based upon the [svelte component template](https://github.com/sveltejs/component-template)
 
 ```bash
-npx degit patrickroberts/svelte-ts-component-template my-new-component
-cd my-new-component
-npm install # or yarn
+npm install --save-dev svelte-gitcal
 ```
 
-Your component's source code lives in `src/Component.svelte`.
+Example usage:
 
-You can create a package that exports multiple components by adding them to the `src` directory and editing `src/index.ts` to reexport them as named exports.
+```js
+<script lang="ts">
+  import GitCal from 'svelte-gitcal'
 
-TODO
+  // generate gitlog with:
+  // $> git log --date=short --pretty=format:%ad | sort | uniq -c > git.log
+  const gitlog = fetch('https://<somewhere>/<gitlog output>').then(r =>
+    r.text()
+  )
+</script>
 
-* [ ] some firm opinions about the best way to test components
-* [ ] update `degit` so that it automates some of the setup work
+{#await gitlog then data}
+  <GitCal gitlog={data} months="12" styles={{ commits0: 'rgb(65,72,89)' }} />
+{/await}
+```
 
-## Setting up
+The calendar lists the number of commits in 4 categories, based upon the max commits per day for the entire period, so commit colors are relative to the max nr of commits (hence the not to clear legend ;-) )
 
-* Run `npm init` (or `yarn init`)
-* Replace this README with your own
+Available styles:
+- commits0: no commits
+- commits1: some commits
+- commits2: more commits
+- commits3: a lot
+- text: style for legend and month texts
 
-## Consuming components
 
-Your package.json has a `"svelte"` field pointing to `src/index.ts`, which allows Svelte apps to import the source code directly, if they are using a bundler plugin like [rollup-plugin-svelte](https://github.com/sveltejs/rollup-plugin-svelte) or [svelte-loader](https://github.com/sveltejs/svelte-loader) (where [`resolve.mainFields`](https://webpack.js.org/configuration/resolve/#resolve-mainfields) in your webpack config includes `"svelte"`). **This is recommended.**
 
-For everyone else, `npm run build` will bundle your component's source code into a plain JavaScript module (`dist/index.mjs`) and a UMD script (`dist/index.js`). This will happen automatically when you publish your component to npm, courtesy of the `prepublishOnly` hook in package.json.
---
+++ src/GitCal.svelte
@@ -0,0 +1,137 @@
+<script lang="ts">
+  // generate gitlogfile:
+  // $> git log --date=short --pretty=format:%ad | sort | uniq -c > git.log
+  export let gitlog: string
+  export let styles = {}
+  export let months = '6'
+
+  const _styles: { [id: string]: string } = {
+    commits0: 'rgb(22,27,34)',
+    commits1: 'rgb(14,68,41)',
+    commits2: 'rgb(38, 166, 65)',
+    commits3: 'rgb(57, 211, 83)',
+    text: 'rgb(201,209,217)',
+    ...styles,
+  }
+
+  let days: { date: Date; commits: number }[] = []
+  let loading = true
+  let maxcommits = 0
+  let error = ''
+
+  const _gitlog: { [id: string]: number } = gitlog
+    .split('\n')
+    .map(d => d.match(/\s+(\d+)\s+(\d{4}-\d{2}-\d{2})/))
+    .reduce((a, c) => ({ ...a, ...(c && { [c[2]]: c[1] }) }), {})
+
+  const setStyles = (node: HTMLElement | SVGElement, styles: object) => {
+    Object.entries(styles).forEach(([key, value]) => {
+      node.style.setProperty(key, value)
+    })
+  }
+
+  // calculate start and stop date
+  const calEnd = new Date()
+  calEnd.setDate(calEnd.getDate() - 1)
+  const calStart = new Date(
+    calEnd.getFullYear(),
+    calEnd.getMonth() - Number(months),
+    1
+  )
+  calStart.setDate(-calStart.getDay() + 1)
+
+  // format days
+  for (let date = calStart; date < calEnd; date.setDate(date.getDate() + 1)) {
+    const day =
+      date.getFullYear() +
+      '-' +
+      ('00' + (date.getMonth() + 1)).slice(-2) +
+      '-' +
+      ('00' + date.getDate()).slice(-2)
+    const commits = _gitlog[day] || 0
+    if (commits > maxcommits) maxcommits = commits
+    days.push({
+      date: new Date(date),
+      commits,
+    })
+  }
+  loading = false
+
+  const commits = (c: number) => {
+    if (c === 0) return 'commits0'
+    if (c / maxcommits <= 0.2) return 'commits1'
+    if (c / maxcommits <= 0.7) return 'commits2'
+    return 'commits3'
+  }
+</script>
+
+{#if loading}
+  Loading ..
+{:else if error}
+  {error}
+{:else}
+  <svg width={((days.length / 7) | 0) * 12 + 8} height={7 * 12 + 2 * 14}>
+    <g>
+      {#each days as day, index}
+        {#if day.date.getDate() === 1}
+          <text
+            use:setStyles={{ fill: _styles.text }}
+            class="text"
+            x={((index / 7) | 0) * 12}
+            y="10"
+          >
+            {day.date.toLocaleString('default', { month: 'short' })}
+            {#if !day.date.getMonth()}
+              -{day.date.getFullYear() % 100}
+            {/if}
+          </text>
+        {/if}
+        <rect
+          fill={_styles[commits(day.commits)]}
+          width="10"
+          height="10"
+          x={((index / 7) | 0) * 12}
+          y={day.date.getDay() * 12 + 14}
+          rx="2"
+          ry="2"
+        >
+          <title
+            >{day.date.toDateString()} - {day.commits === 0
+              ? 'no '
+              : day.commits}
+            commit{day.commits !== 1 ? 's' : ''}</title
+          >
+        </rect>
+      {/each}
+      {#each ['none', 'some', 'more', 'a lot'] as text, index}
+        {index}
+        <rect
+          fill={_styles['commits' + index]}
+          width="10"
+          height="10"
+          x={index * 45}
+          y={14 + 7 * 12 + 2}
+          rx="2"
+          ry="2"
+        >
+          <title>{text}</title>
+        </rect>
+        <text
+          use:setStyles={{ fill: _styles.text }}
+          class="text"
+          x={index * 45 + 12 + 2}
+          y={14 + 7 * 12 + 10}
+        >
+          {text}
+        </text>
+      {/each}
+      >
+    </g>
+  </svg>
+{/if}
+
+<style>
+  .text {
+    font-size: xx-small;
+  }
+</style>
